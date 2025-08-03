1b68c38bcaf8b9e3b5b3577594c12a1be9730d82
Godamker <2323610@cognizant.com>
2025-08-03 18:22:54 +0530

    Update for Railway deployment and config

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, redirect, url_for, flash, request, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
@@ -87,8 +88,8 @@ def load_clipboard():
     return {'content': clipboard.content if clipboard else ''}
 
 if __name__ == '__main__':
+    # For local development only: create tables and run Flask dev server
     with app.app_context():
         db.create_all()
-        port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-    # app.run(debug=True)
