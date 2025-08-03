882f71e478b002d6e0790452bd71c55ad92e7359
Yashasya <79990866+yashasya08@users.noreply.github.com>
2025-08-03 17:19:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -89,4 +89,6 @@ def load_clipboard():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+        port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    # app.run(debug=True)
